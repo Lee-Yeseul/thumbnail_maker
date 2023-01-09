636d078949c7b38e888363b84a9ddc8b6e6c167e
Lee-Yeseul <ssulv3030@gmail.com>
2023-01-09 22:03:02 +0900

    setting: 초기설정(prettier, eslint, husky)

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,17 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { resolve } from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
     alias: [
-      { find: "@src", replacement: resolve(__dirname, "src") },
+      { find: '@src', replacement: resolve(__dirname, 'src') },
       {
-        find: "@components",
-        replacement: resolve(__dirname, "src/components"),
+        find: '@components',
+        replacement: resolve(__dirname, 'src/components'),
       },
-      { find: "@styles", replacement: resolve(__dirname, "src/styles") },
+      { find: '@styles', replacement: resolve(__dirname, 'src/styles') },
     ],
   },
 });
