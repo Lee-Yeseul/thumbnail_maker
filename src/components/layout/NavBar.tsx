b8af58abdc38221025c9b8cf38ed4f071d6faea5
Lee-Yeseul <ssulv3030@gmail.com>
2023-01-15 22:03:41 +0900

    style : tailwind 관련 코드 삭제

--- src/components/layout/NavBar.tsx
+++ src/components/layout/NavBar.tsx
@@ -2,9 +2,9 @@ import { Link } from 'react-router-dom';
 
 export default function NavBar() {
   return (
-    <div className="flex w-full justify-center space-x-5 ">
+    <div>
       <Link to="/">
-        <h1>Home</h1>
+        <h1>Blog</h1>
       </Link>
       <Link to="/notion">
         <h1>Notion</h1>
